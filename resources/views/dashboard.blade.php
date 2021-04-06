c7f0df37d63a2fde2c8de5209fc658ab36098bf0
Edson Jordan <edson_4555@hotmail.com>
2021-04-06 11:15:57 -0500

    Como modificar la plantilla de Jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{-- {{ __('Dashboard') }} --}} Sunqupacha
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                {{-- <x-jet-welcome /> --}}
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis quos molestiae facilis dolorem, dolor soluta sequi qui unde incidunt dolorum debitis a vitae quasi et nulla assumenda totam! Eius, architecto.</p>
+                <p>Beatae aperiam architecto odio unde non mollitia. Quisquam eveniet commodi soluta voluptas nobis quo, facilis illo eligendi doloremque delectus fuga laudantium enim quaerat praesentium aperiam asperiores consequuntur. Ullam, ratione accusamus.</p>
+                <p>Accusamus, vero minima debitis dignissimos a unde non praesentium consequuntur quos. Veniam ullam et obcaecati non numquam eligendi ea eius, commodi tempora rem officia eos voluptatibus at quaerat praesentium nesciunt.</p>
+                <p>Neque magni, aut, ut enim deserunt ex dolore aperiam atque, voluptatibus repellendus nisi ipsam quidem consectetur fugit repudiandae distinctio laboriosam possimus tenetur? At a fuga expedita mollitia quibusdam ratione ipsum!</p>
+                <p>Necessitatibus quidem ea eligendi. Culpa molestias rerum sunt necessitatibus molestiae? Quam quia enim eveniet et dolores impedit necessitatibus repellendus. Itaque odio voluptatum a sint quo veritatis maiores voluptatem est voluptas?</p>
             </div>
         </div>
     </div>
