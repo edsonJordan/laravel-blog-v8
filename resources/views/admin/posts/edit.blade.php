5a7f0a81888cbf75f9af4e0d134685fe9f397743
Edson Jordan <edson_4555@hotmail.com>
2021-05-04 22:59:06 -0500

    añadiendo cambios de imagen

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,13 +8,70 @@
 @stop
 
 @section('content')
-    <p>Bienvenido al panel de administración</p>
+    <div class="card">
+        <div class="card-body">
+            {!! Form::model($post, ['route' => 'admin.posts.store', 'autocomplete' => 'off', 'files' => true]) !!}
+
+                {!! Form::hidden('user_id', auth()->user()->id) !!}
+                @include('admin.posts.partials.form')
+                {!! Form::submit('Crear Post', ['class' => 'btn btn-primary btn-sm']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <style>
+        .image-wrapper{
+            position: relative;
+            padding-bottom: 56.25%;
+        }
+        .image-wrapper img{
+            position: absolute;
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
+
+    </style>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <script src="{{ asset('vendor/StringToSlug/jquery.stringToSlug.min.js') }}" ></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
+    <script>
+        $(document).ready( function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+        
+        ClassicEditor
+        .create( document.querySelector( '#stract' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+
+        ClassicEditor
+        .create( document.querySelector( '#body' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+
+        document.getElementById("file").addEventListener('change', cambiarImagen);
+
+        function cambiarImagen(event){
+            var file = event.target.files[0];
+
+            var reader = new FileReader();
+            reader.onload = (event) => {
+                document.getElementById("picture").setAttribute('src', event.target.result);
+            };
+
+            reader.readAsDataURL(file);
+        }
+
+    </script>
+@endsection
